a75693158e79bf7c786830d69748ab64a42e85af
HFproject <92003043+HFproject@users.noreply.github.com>
2022-08-24 10:12:00 -0500

    Add files via upload

--- Procesamiento/SendingScripts/send_SCP.py
+++ Procesamiento/SendingScripts/send_SCP.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env
 import os,glob,datetime
+from datetime import date
 import argparse
 import pexpect #pip install --user  pexpect
 import time
+import calendar
 # En caso no se pueda enviar hacer escribir etas lineas >
 
 #ssh -X -C wmaster@jro-app.igp.gob.pe -p 6633
@@ -24,7 +26,18 @@ def sendBySCP(Incommand, location): #"%04d/%02d/%02d 00:00:00"%(tnow.year,tnow.m
 	command = Incommand
 	if debug:
 		print(command)
+
+	console = pexpect.spawn('/bin/bash', ['-c',command])
+	
+	if not (location == 11 or location == 12):
+		console.expect(username + "@" + host + "'s password:")			
+		time.sleep(3)
+		#usual response > wmaster@jro-app.igp.gob.pe's password:
+		console.sendline(password)
+		
+	time.sleep(5)
 	try:
+		os.system(command) ## La magia
 		#pexpect.spawn doesn't interpret shell meta characters,
 		#console = pexpect.spawn(command)
 		#Para reconocerlos se debe usar la siguiente linea >
@@ -35,13 +48,199 @@ def sendBySCP(Incommand, location): #"%04d/%02d/%02d 00:00:00"%(tnow.year,tnow.m
 			time.sleep(3)
 			#usual response > wmaster@jro-app.igp.gob.pe's password:
 			console.sendline(password)
-		time.sleep(7)
+		time.sleep(5)
 		#console.expect(pexpect.EOF)
 		return True
-	except(Exception, e):
+	except(Exception):
 		if debug:
-			print(str(e))
+			print("Error")
+			#print(str())
 		return False
+
+def notdays(ruta_days):
+    year=int(ruta_days[0][-10:-6])
+    month = int(ruta_days[0][-5:-3])
+
+    #print("Year: ",year,"Month: ", month)
+    DaysInMonth = calendar.monthrange(year,month)[1]
+
+    days_real = list(range(1,DaysInMonth+1))
+    days_Rx = [int(i[-2:]) for i in ruta_days]
+
+    days_fal =[d for d in days_real if not(d in days_Rx)]
+    daysOfYear_fal = [int(date(year,month,i).strftime('%j')) for i in days_fal]
+    sorted(daysOfYear_fal)
+    return daysOfYear_fal
+
+def incompletedays(data, rxcode,out_or_figure):
+    #data_in must be  a list tlike this:
+    year= int(data[0][-10:-6])
+    month= int(data[0][-5:-3])
+    sorted(data)
+    days_incomplete = []
+    for ruta_dia in data:
+        datos = glob.glob(ruta_dia+'/%s/*.%s'%(out_or_figure,out_or_figure))
+        sorted(datos)
+        if rxcode in [11,12,21,22]:
+            if len(datos) != 4:
+                day_inc = int(ruta_dia[-2:])
+                days_incomplete.append(day_inc)
+
+        else:
+            if len(datos) != 2:
+                day_inc = int(ruta_dia[-2:])
+                days_incomplete.append(day_inc)
+    sorted(days_incomplete)
+
+    daysOfYear_inc = [int(date(year,month,i).strftime('%j')) for i in days_incomplete]
+    return daysOfYear_inc
+
+def File_writer(dias_faltantes,station_type,datatype,year,rxcode):
+    print("YEAR Fx: ",year)
+    Tx_s = list(dias_faltantes.keys())
+    print("Lista de entrada: ",Tx_s)
+    cont=0
+    #Comparacion de elementos faltantes en las listas que si lo contienen
+    for t in Tx_s:
+        print("")
+        print("Para ",t)
+        Tx_v = Tx_s.copy()
+        Tx_v.remove(t)
+        for t_v in Tx_v:
+            print("revisar,",t_v)
+            if cont == 0:
+                days_f =  [d for d in dias_faltantes[t] if not (d in dias_faltantes[t_v])]
+                cont += 1
+            else:
+                days_f +=  [d for d in dias_faltantes[t] if not (d in dias_faltantes[t_v])]
+                cont += 1
+            
+    days_f= sorted(list(set(days_f)))
+    print("Days_f: ",days_f)
+    #Elegir el maximo
+    for d in days_f:
+        print("d general: ",d)
+        for i in range(len(Tx_s)):
+            print("")
+            print("d:",d, not (d in dias_faltantes[Tx_s[i]]))
+            print(Tx_s[i],"BEFORE",dias_faltantes[Tx_s[i]],sep=" ")
+            if not (d in dias_faltantes[Tx_s[i]]):
+                #print("Lista de dia faltante: ",dias_faltantes[Tx_s[i]])
+                cont_n=-1
+                for j in dias_faltantes[Tx_s[i]]:
+                    
+                    indice = dias_faltantes[Tx_s[i]].index(j)
+                    a_None = [indice for indice, dato in enumerate(dias_faltantes[Tx_s[i]]) if dato == None]
+                    
+                    try:
+                        if j > d and dias_faltantes[Tx_s[i]][indice-1] < d:
+                        
+                            #indice = dias_faltantes[Tx_s[i]].index(j)
+                            #print("\n")
+                            #print("Dia faltante: ",d)
+                            dias_faltantes[Tx_s[i]].insert(indice,None)
+                            #print(Tx_s[i]," AFTER: ",dias_faltantes[Tx_s[i]],sep=" ")
+                            break
+                    except:
+                        if j == None:
+                            cont_n += 1
+
+                            if dias_faltantes[Tx_s[i]][a_None[cont_n]+1] == None:
+                                continue
+                            else:
+                                if dias_faltantes[Tx_s[i]][a_None[cont_n]+1] > d:
+                                    dias_faltantes[Tx_s[i]].insert(a_None[cont_n]+1,None)
+                                    #print(Tx_s[i]," AFTER: ",dias_faltantes[Tx_s[i]],sep="\n")
+                                    break
+                                else:
+                                    continue
+    #print(rxcode)
+    print(dias_faltantes)
+
+    for i in range(len(Tx_s)):
+        print(Tx_s[i],":",len(dias_faltantes[Tx_s[i]]))
+        for j in range(len(dias_faltantes[Tx_s[i]])):
+            if dias_faltantes[Tx_s[i]][j] == None:
+                continue
+            dias_faltantes[Tx_s[i]][j] = year*1000+dias_faltantes[Tx_s[i]][j]
+    try:
+        os.remove("Doys_not_sent_%s_%s"%(datatype,station_type))
+    except:
+        os.system("touch Doys_not_sent_%s_%s"%(datatype,station_type))
+    #os.remove("Doys_not_sent_%s_%s"%(datatype,station_type))
+    long = len(dias_faltantes[Tx_s[0]])
+    
+    with open("Doys_not_sent_%s_%s"%(datatype,station_type), "w") as f:
+        print(list(dias_faltantes.keys()))
+        codigos = [0,1,2]
+        for i in range(long):
+            lista = [ dias_faltantes[t][i] for t in list(dias_faltantes.keys()) ]
+            #print(lista)
+            if rxcode in [11,12,21,22]: #Estacion doble
+                f.write(str(lista[0])+','+str(lista[1])+','+str(lista[2])+"\n") #File contains
+            else:  #Estacion simple
+                lista2 = []
+                for i in codigos:
+                    if lista[2*i+0] ==  lista[2*i+1] == None:
+                        lista2.append(lista[2*i+0])
+                    elif lista[2*i+0] != None:
+                        lista2.append(lista[2*i+0])
+                    elif lista[2*i+1] != None:
+                        lista2.append(lista[2*i+1])
+                
+                f.write(str(lista2[0])+','+str(lista2[1])+','+str(lista2[2])+"\n") #File contains
+
+def dias_faltantes(ruta_base, rxcode,rxname,datatype):
+    #station_rx = rxname
+    station_tx =  ['HFTXANCON','HFBTXANCON','HFTXSICAYA','HFBTXSICAYA','HFTXICA','HFBTXICA']
+    station_orientation = "A" if rxcode % 10 == 1 else "B"
+    out_or_figure = "out" if datatype == "out" else "figures"
+    AorB = rxcode%2
+
+    Days_off_tx = {}
+    Days_inc_tx = {}
+
+    date_year = date.today().year
+    date_month = date.today().month
+    date_day = date.today().day
+    if rxcode in [11,12,21,22]:
+        if not(AorB):
+            tx_s = [ i for i in station_tx if i[:3]== 'HFB']
+        else:
+            tx_s = [ i for i in station_tx if i[:3]== 'HFT']
+    else:
+        tx_s = station_tx
+        station_orientation = "A"
+    
+    dias_faltantes = {}
+
+    for tx in tx_s:
+        print("*****",tx,"*****")
+        ruta_tx = ruta_base+rxname+"/"+tx+'/'+str(date_year)+"/"
+        days_off=[]
+        days_incomplete = []
+        for month in range(1,date_month+1):
+            if len(str(month)) == 1:
+                month = '0'+str(month)
+            else:
+                month=str(month)
+            print("")
+            print("MES: ",month)
+            ruta_mes = ruta_tx+month+'/'
+            datos_dias=glob.glob(ruta_mes+'/*')
+            sorted(datos_dias)
+            days_off += notdays(datos_dias)
+            Days_off_tx[tx]=days_off
+
+            days_incomplete += incompletedays(datos_dias, rxcode,out_or_figure)
+            Days_inc_tx[tx] = days_incomplete
+            Days_off_tx[tx] = sorted(days_off + days_incomplete)
+
+    print("")
+    print("Dias de datos incompletos:",Days_off_tx)
+    File_writer(Days_off_tx,station_orientation,datatype,date_year,rxcode)
+
+
 
 ################################### INGRESANDO CODIGO 0 o 2 ##############################################################
 parser = argparse.ArgumentParser()
@@ -62,10 +261,34 @@ tdstr = datetime.date.today()
 str1 = tdstr + datetime.timedelta(days=-1)
 yesterday = str1.strftime("%Y%j")
 
+ruta_base = "/data/hfdatabase/webhf/web_rtdi/data/"
+
 dlist = []
 file_lines = []
 station_type = "A" if rxcode % 10 == 1 else "B"
 print("***",datatype,station_type)
+
+location_dict = {11:"JRO_A", 12: "JRO_B", 21:"HYO_A", 22:"HYO_B", 31:"MALA",
+	41:"MERCED", 51:"BARRANCA", 61:"OROYA"}
+
+if rxcode == 11 or rxcode == 12 :
+	rxname = 'JRO'
+if rxcode == 21 or rxcode == 22 :
+	rxname = 'HUANCAYO'
+if rxcode == 31 or rxcode == 32 :
+	rxname = 'MALA'
+if rxcode == 41 or rxcode == 42 :
+	rxname = 'MERCED'
+if rxcode == 51 or rxcode == 52 :
+	rxname = 'BARRANCA'
+if rxcode == 61 or rxcode == 62 :
+	rxname = 'OROYA'
+
+
+###Funcion para crear los archivos de dias faltantes
+#dias_faltantes(ruta_base, rxcode,rxname,datatype)
+
+
 with open("Doys_not_sent_%s_%s"%(datatype,station_type),"r") as f:
 	# The Doys_not_sent file have data about the doys that hasn't been sent, each line represents a doys
 	# And each line has the same doy or None for each code, None represents that the data hasn't
@@ -82,8 +305,7 @@ if yesterday not in [doy for line in file_lines for doy in line]:
 	file_lines.append([yesterday]*3)
 
 
-location_dict = {11:"JRO_A", 12: "JRO_B", 21:"HYO_A", 22:"HYO_B", 31:"MALA",
-	41:"MERCED", 51:"BARRANCA", 61:"OROYA"}
+
 if rxcode in [11, 12, 21, 22]:
 	station_orientation = "A" if rxcode % 10 == 1 else "B"
 else:
@@ -104,6 +326,8 @@ else:
 	graph_freq1 = PATH+"Pictures/RTDI_%s/graphics_schain/"%station_orientation + 'sp'+str(code)+'1_f1'
 print("")
 print("GRAPH_FREQ0: ",graph_freq0)
+print("GRAPH_FREQ1: ",graph_freq1)
+print("")
 str_datatype = "*/" if datatype == "params" else ""
 
 web_type = "web_signalchain" if datatype == "params" else "web_rtdi"
@@ -114,28 +338,38 @@ out_or_figures = "out" if datatype == "out" else "figures"
 for file in dlist[:]: # Se usa una copia porque dlist puede ser modificado dentro del bucle
 	data_written = True #Flag that represents data_written
 	doy = 'd'+file
-	jpg_files = glob.glob("%s/%s/%s*.jpeg"%(graph_freq1, doy,str_datatype))
+	
+	#jpg_files = glob.glob("%s/%s/%s*.jpeg"%(graph_freq1, doy,str_datatype))
+	arch_files= glob.glob("%s/%s/%s*.%s"%(graph_freq0, doy,str_datatype,extension))
+	arch_files_str= "%s/%s/%s*.%s"%(graph_freq0, doy,str_datatype,extension)
 	#print("PRINTEA: ","%s/%s/%s*.jpeg"%(graph_freq1, doy,str_datatype))
-	jpg_files.sort()
-	#print("LISTA",jpg_files)
-	print("%s/%s/%s*.jpeg"%(graph_freq1, doy,str_datatype))
-	if len(jpg_files) is 0:
-		print('No hay RESULTADOS en la carpeta!!!')
+
+	#jpg_files.sort()
+	arch_files.sort()
+	print("LISTA",arch_files)
+	#print("%s/%s/%s*.jpeg"%(graph_freq1, doy,str_datatype))
+	#if len(jpg_files) is 0:
+	if len(arch_files) is 0:
+		print('No hay RESULTADOS en la carpeta %s !!'%(arch_files_str))
+		print("")
 		continue
 	#file_1=os.path.basename(jpg_files[0])[offset:]
 	if datatype == "params":
-		file_1 = jpg_files[0][:-1].split("/")[-3]
+		#file_1 = jpg_files[0][:-1].split("/")[-3]
+		file_1 = arch_files[0][:-1].split("/")[-3]
 	else:
-		file_1 = jpg_files[0][:-1].split("/")[-2]
+		#file_1 = jpg_files[0][:-1].split("/")[-2]
+		file_1 = arch_files[0][:-1].split("/")[-2]
 	file_1 = file_1[1:]
+	print("FILE: ",file_1)
 
 	YEAR=int (file_1[0:4])
 	DAYOFYEAR= int(file_1[4:7])
 	DAYOFYEAR_str = file_1[4:7]
 	d = datetime.date(YEAR, 1, 1) + datetime.timedelta(DAYOFYEAR - 1)
 	print("(1) Determinando dia a enviar.")
-	print('d.strftime("%Y/%m/%d")>', d.strftime("%Y/%m/%d"))
-
+	#print('d.strftime("%Y/%m/%d")>', d.strftime("%Y/%m/%d"))
+	print("Fecha: ",d.strftime("%Y/%m/%d"))
 	MONTH = d.strftime("%m")
 	DAY = d.strftime("%d")
 	AorB = rxcode%2 # Si es par(B) AorB sera 0, si es impar(A) AorB sera 1.
@@ -169,12 +403,14 @@ for file in dlist[:]: # Se usa una copia porque dlist puede ser modificado dentr
 
 	#remote_folder="/home/wmaster/web2/web_rtdi/data/JRO/%s/%s/%s/%s/figures/"%(station_name, YEAR, MONTH,DAY)
 	remote_folder="/home/wmaster/web2/%s/data/%s/%s/%s/%s/%s/%s/"%(web_type,rxname,station_name, YEAR, MONTH,DAY, out_or_figures)
+	print("")
 	print("(2) Direccion de llegada de datos.")
 	print("Remote_folder: %s"%(remote_folder))
+	print("")
 	#Primer Comando, generar carpeta de destino.
 
 	command_1="ssh wmaster@jro-app.igp.gob.pe -p 6633 mkdir -p %s"%(remote_folder)
-	print(sendBySCP(command_1, rxcode))
+	print("HOLITAS",sendBySCP(command_1, rxcode))
 	if sendBySCP(command_1, rxcode):
 		print('Carpeta Creada')
 
@@ -183,24 +419,32 @@ for file in dlist[:]: # Se usa una copia porque dlist puede ser modificado dentr
 
 	#Segundo comando, pasar las imagenes necesarias para la freq 0
 	print(" (3) Enviando resultados frecuencia 0")
-	temp_command = "scp -r -P 6633 %s/%s/%s%s%s%s%s*.%s wmaster@jro-app.igp.gob.pe:%s"%(graph_freq0,doy,str_datatype,
-	letter,YEAR, DAYOFYEAR_str, rxcode, extension, remote_folder)
+	#print("%s/%s/%s%s%s%s%s*.%s wmaster@jro-app.igp.gob.pe:%s"%(graph_freq0,doy,str_datatype,
+	#letter,YEAR, DAYOFYEAR_str, rxcode, extension, remote_folder))
+	
 
-	if sendBySCP(temp_command, rxcode):
+	temp_command0 = "scp -r -P 6633 %s/%s/%s%s%s%s%s*.%s wmaster@jro-app.igp.gob.pe:%s"%(graph_freq0,doy,str_datatype,
+	letter,YEAR, DAYOFYEAR_str, rxcode, extension, remote_folder)
+	#print("Comando 01: ", temp_command0)
+	#print(sendBySCP(temp_command0, rxcode))
+	if sendBySCP(temp_command0, rxcode):
 		print(' -- Datos Enviados F0')
 	else:
 		data_written=False
+		print("Datos no se enviaron.")
 
 	#Segundo comando, pasar las imagenes necesarias para la freq 1					AQUI
 	#temp_command = "scp -r -P 6633 %s/%s/*.jpeg wmaster@jro-app.igp.gob.pe:%s"%(graph_freq1,doy,remote_folder)
 	print("(4) Enviando resultados frencuencia 1") #DAYOFYEAR_str
-	temp_command = "scp -r -P 6633 %s/%s/%s%s%s%s%s*.%s wmaster@jro-app.igp.gob.pe:%s"%(graph_freq1,doy,str_datatype,
+	temp_command1 = "scp -r -P 6633 %s/%s/%s%s%s%s%s*.%s wmaster@jro-app.igp.gob.pe:%s"%(graph_freq1,doy,str_datatype,
 	letter,YEAR, DAYOFYEAR_str, rxcode, extension, remote_folder)
+	#print(temp_command1)
 
-	if sendBySCP(temp_command, rxcode):
+	if sendBySCP(temp_command1, rxcode):
 		print(' -- Datos enviados F1 ')
 	else:
 		data_written = False
+		print("Datos no se enviaron.")
 
 	##Checks if all data where sent, if not save the doy in the file what contains all doy not sent yet.
 	if data_written:
