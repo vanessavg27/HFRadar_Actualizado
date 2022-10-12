4b891f51b163d0906b6cd037928f0e4624b6d19b
HFproject <92003043+HFproject@users.noreply.github.com>
2022-10-12 10:15:49 -0500

    Add files via upload

---
+++ Recepcion/proc_f0_multiple.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "PROC_F0"
+
+while true;do python3 hf_proc_gdf2hdf5_multiple.py  --station-name=HF --freq=f0 -N 90 -x 10 --rawdata-from-doy=/media/soporte/RAWDATA/ --procdata=/media/soporte/PROCDATA/  --graphics=file/media/igp-114/PROCDATA/ --send-graphs=0 --auto-delete-rawdata=1 -I $2 --mode-campaign=$1 --stationtx-codes=$3;done
+
+###### Modo normal ######
+#while true;do python3 hf_proc_gdf2hdf5_multiple.py  --station-name=HF --freq=f0 -N 90 -x 10 --rawdata-from-doy=/media/soporte/RAWDATA/  --procdata=/media/soporte/PROCDATA/  --graphics=file/media/igp-114/PROCDATA/ --send-graphs=0 --auto-delete-rawdata=1 -I 6 --mode-campaign=0 --stationtx-codes="0,1,2";done
+
+
+###### Modo campaña ######
+#while true;do python3 hf_proc_gdf2hdf5_multiple.py  --station-name=HF --freq=f0 -N 90 -x 10 --rawdata-from-doy=/media/soporte/RAWDATA/  --procdata=/media/soporte/PROCDATA/  --graphics=file/media/igp-114/PROCDATA/ --send-graphs=0 --auto-delete-rawdata=1 -I 0 --mode-campaign=1 --stationtx-codes="0,1,2";done  
+
+
+
