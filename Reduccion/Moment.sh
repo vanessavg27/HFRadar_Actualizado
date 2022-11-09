4dd1b28a99f59c7ba06dee9c7a37d512d0f97e3b
HFproject <92003043+HFproject@users.noreply.github.com>
2022-11-09 09:33:16 -0500

    Add files via upload

--- Reduccion/Moment.sh
+++ Reduccion/Moment.sh
@@ -1,9 +1,45 @@
 #/bin/bash
 
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 2.72216796875 -C 1 -code 0 -lo 21 -date 2022/08/29 -R 1
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 2.7221646392718 -C 1 -code 1 -lo 21 -date 2022/08/29 -R 1
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 2.7221712982282 -C 1 -code 2 -lo 21 -date 2022/08/29 -R 1
+#Normal mode  :  ./Moment.sh "/media/soporte/RAWDATA/HYOa/" 21 0 "2022/08/20"
+#Campaign mode:  ./Moment.sh "/media/soporte/RAWDATA/HYOa/" 21 1 "2022/08/29"
 
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 3.64990234375 -C 1 -code 0 -lo 21 -date 2022/08/29 -R 1
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 3.6499056732282 -C 1 -code 1 -lo 21 -date 2022/08/29 -R 1
-python3 Moment.py -path "/media/soporte/RAWDATA/HYOa/" -f 3.6498990142718 -C 1 -code 2 -lo 21 -date 2022/08/29 -R 1
+#Modo normal
+if [ $3 = "0" ]
+then
+	echo "Modo normal"
+	echo "HFA0_C0"
+	python3 Moment.py  -path $1 -f 2.72216796875   -C 0  -code 0 -lo $2 -date $4 -R 1
+	echo "HFA0_C1"
+	python3 Moment.py  -path $1 -f 2.7221646392718 -C 0  -code 1 -lo $2 -date $4 -R 1
+	echo "HFA0_C2"
+	python3 Moment.py  -path $1 -f 2.7221712982282 -C 0  -code 2 -lo $2 -date $4 -R 1
+
+
+	echo "HFA1_C0"
+	python3 Moment.py  -path $1 -f 3.64990234375   -C 0  -code 0 -lo $2 -date $4 -R 1
+	echo "HFA1_C1"
+	python3 Moment.py  -path $1 -f 3.6499056732282 -C 0  -code 1 -lo $2 -date $4 -R 1
+	echo "HFA1_C2"
+	python3 Moment.py  -path $1 -f 3.6498990142718 -C 0  -code 2 -lo $2 -date $4 -R 1
+#Campaña
+elif [ $3 = "1" ]
+then
+	echo "Modo campaña"
+	echo "HFA0_C0"
+	python3 Moment.py  -path $1 -f 2.72216796875   -C 1  -code 0 -lo $2 -date $4 -R 1
+	echo "HFA0_C1"
+	python3 Moment.py  -path $1 -f 2.7221646392718 -C 1  -code 1 -lo $2 -date $4 -R 1
+	echo "HFA0_C2"
+	python3 Moment.py  -path $1 -f 2.7221712982282 -C 1  -code 2 -lo $2 -date $4 -R 1
+
+
+	echo "HFA1_C0"
+	python3 Moment.py  -path $1 -f 3.64990234375   -C 1  -code 0 -lo $2 -date $4 -R 1
+	echo "HFA1_C1"
+	python3 Moment.py  -path $1 -f 3.6499056732282 -C 1  -code 1 -lo $2 -date $4 -R 1
+	echo "HFA1_C2"
+	python3 Moment.py  -path $1 -f 3.6498990142718 -C 1  -code 2 -lo $2 -date $4 -R 1
+else
+	echo "Error en flag de campaña en Moments.sh"
+	exit 1
+fi
