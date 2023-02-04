161a009c776eaa85546dcd1120eacf613f7fa4ed
HFproject <92003043+HFproject@users.noreply.github.com>
2023-02-04 17:12:34 -0500

    Add files via upload

--- script_moments.sh
+++ script_moments.sh
@@ -6,28 +6,36 @@
 
 #./script_moments.sh 11 1 r 
 
-#export DISPLAY=":0.0"  #se comento por falla de generacion de plots
-
 #El primer parametro de todos los scripts llamados es la ubicacion de la data y el segundo es la locacion de la estacion
 #El tercero es un flag de campaña,1 campaña, 0 modo normal
 
 #se pasa esa fecha si no pone por defecto la fecha del dia anterior
+#${3:-$(date -d "yesterday" +"%Y/%m/%d")}
+date=$4
+date_to_process=$(date -d "yesterday" +"%Y/%m/%d") #Comentar si se desea procesar otra fecha
+date_to_process="${date:-$date_to_process}"
+#date_to_process="2022/08/29" # Modificar y quitar el comentario si se desea procesar otra fecha
+echo "date" $date_to_process
 
-#date_to_process=$(date -d "yesterday" +"%Y/%m/%d") #Comentar si se desea procesar otra fecha
-date_to_process="2022/08/31" # Modificar y quitar el comentario si se desea procesar otra fecha
+###### VARIABLES ######
+ruta_procdata="/media/soporte/PROCDATA/"
 
+#########################CONDICIONAL###############################
 if [[ $3 == 'r' ]]
 
 then
-############################################################# REDUCE SPEC-DATA #################################################
+####################### REDUCE SPEC-DATA ##########################
 cd $HOME/Actualizado/Reduccion/
 echo "Starting Reduction!"
-screen -S "REDUCTED HF" -d -m ./Filtrado.sh "/media/soporte/Data/" $2 $date_to_process
+#ruta_especreduced="/media/soporte/PROCDATA/"
+ruta_especreduced="/media/soporte/PROCDATA/MERCED/"
+echo "./Filtrado.sh" $ruta_procdata $2 $date_to_process $ruta_especreduced
+./Filtrado.sh $ruta_procdata $2 $date_to_process $ruta_especreduced
 
 sleep 5
 ############ Variables ############
 reduct=1
-ruta_moments="/media/soporte/PROCDATA/MomentsFloyd_Filt/"
+ruta_moments="/media/soporte/PROCDATA/MERCED/MomentsFloyd_Filt/"
 
 else
 ############ Variables ############
@@ -38,7 +46,10 @@ cd $HOME/Actualizado/Procesamiento/Plottings/
 fi
 ########################################################### Generating Moments Data #################################################
 echo "Starting Moments"
-screen -S "MOMENTS_HF" -d -m ./Moment.sh "/media/soporte/PROCDATA/" $1 $2 $date_to_process $reduct
+#screen -S "MOMENTS_HF" -d -m 
+echo "reduct" $reduct
+echo "./Moment.sh" "/media/soporte/PROCDATA/" $1 $2 $date_to_process $reduct
+./Moment.sh "/media/soporte/PROCDATA/" $1 $2 $date_to_process $reduct
 
 sleep 1
 #Espera a que los nuevos archivos de momentos hayan terminado de generarse
@@ -53,12 +64,14 @@ done
 ######################################################### Plotting Parameters SNR, DOPLLER y OUT #################################################3
 cd $HOME/Actualizado/Procesamiento/Plottings/
 echo "Plotting Plottings out"
-screen -S "PlottingParam_Floyd" -d -m ./Plot_RTDI_OUT.sh $ruta_moments $1 $2 $date_to_process
+#screen -S "PlottingParam_Floyd" -d -m 
+echo "./Plot_RTDI_OUT.sh" $ruta_moments $1 $2 $date_to_process
+./Plot_RTDI_OUT.sh $ruta_moments $1 $2 $date_to_process
 sleep 1
 COUNT=$(screen -list | grep -c "PlottingParam_Floyd")
 while [  $COUNT != "0" ]
 do
-	sleep 3
+
 	echo "Waiting for PlottingParam RTDI Out to finish"
 	COUNT=$(screen -list | grep -c "PlottingParam")
 done
@@ -66,7 +79,8 @@ done
 ###################Plotting RTDI#################################################3
 
 echo "Plotting the new RTDI from moments"
-screen -S "PLOT_RTDI" -d -m ./PLOT_RTDI.sh $ruta_moments $1 $2 $date_to_process
+#screen -S "PLOT_RTDI" -d -m 
+./PLOT_RTDI.sh $ruta_moments $1 $2 $date_to_process
 
 sleep 1
 COUNT=$(screen -list | grep -c "PLOT_RTDI")
