bcd3d224bc8fd1f96c86f101500a8aef39e4dfdc
“HF-RADAR” <vaneevasquezg@gmail.com>
2024-05-10 14:01:53 -0500

    Delete any files

--- Monitoreo/GmailTutorial/bla.py
+++
@@ -1,3 +0,0 @@
-uhd_image_loader --args="usrp2,addr=192.168.10.3,overwrite-safe" --fw-path="/usr/share/uhd/images/usrp_n200_fw.bin" --fpga-path="/usr/share/uhd/images/usrp_n200_r4_fpga.bin"
-
-sudo ./usrp2_card_burner.py 
